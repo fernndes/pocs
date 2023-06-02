642b59abe49201b4abcba7adb4668fc3dcf7ee6b
fernndes <ferndes.gabriel@hotmail.com>
2023-06-01 21:20:47 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- React/react-clean-solid-tdd/src/domain/test/mock-account.ts
+++ React/react-clean-solid-tdd/src/domain/test/mock-account.ts
@@ -1,6 +1,11 @@
 import { AuthenticationParams } from "@/domain/usecases/authentication";
+import { AccountModel } from "../models/account-model";
 
 export const mockAuthentication = (): AuthenticationParams => ({
     email: 'teste@teste.com',
     password: '123456789'
+})
+
+export const mockAccountModel = (): AccountModel => ({
+    accessToken: 'fgasdgsadgdfbafdgdf5g4adsf5g4sad4g5sdg4'
 })
