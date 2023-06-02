5c28d7d2fbb14d8153d21e94137775796ac3aac4
fernndes <ferndes.gabriel@hotmail.com>
2023-06-01 21:29:55 -0300

    refactor: group imports in index files

--- React/react-clean-solid-tdd/src/domain/test/mock-account.ts
+++ React/react-clean-solid-tdd/src/domain/test/mock-account.ts
@@ -1,5 +1,5 @@
-import { AuthenticationParams } from "@/domain/usecases/authentication";
-import { AccountModel } from "../models/account-model";
+import { AuthenticationParams } from "@/domain/usecases";
+import { AccountModel } from "../models";
 
 export const mockAuthentication = (): AuthenticationParams => ({
     email: 'teste@teste.com',
