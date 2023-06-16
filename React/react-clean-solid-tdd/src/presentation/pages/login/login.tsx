46fd01798524394639a0b3501fca20507995ec7d
fernndes <ferndes.gabriel@hotmail.com>
2023-06-16 15:11:29 -0300

    feat: ensure login calls validation with correct password

--- React/react-clean-solid-tdd/src/presentation/pages/login/login.tsx
+++ React/react-clean-solid-tdd/src/presentation/pages/login/login.tsx
@@ -13,6 +13,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
         isLoading: false,
         mainError: '',
         email: '',
+        password: '',
         emailError: 'Campo obrigatório',
         passwordError: 'Campo obrigatório',
     })
@@ -20,6 +21,10 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     useEffect(() => {
         validation.validate({ email: state.email })
     }, [state.email])
+
+    useEffect(() => {
+        validation.validate({ password: state.password })
+    }, [state.password])
 
     return (
         <div className={Styles.login}>
