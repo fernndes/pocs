e0379ad2a3352b240ae56df555e3b828a704661b
fernndes <ferndes.gabriel@hotmail.com>
2023-06-16 13:42:50 -0300

    test: ensure login inputs starts with required state

--- React/react-clean-solid-tdd/src/presentation/pages/login/login.spec.tsx
+++ React/react-clean-solid-tdd/src/presentation/pages/login/login.spec.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
 import { render } from '@testing-library/react'
 import Login from './login'
 
 describe('Login Component', () => {
+
     test('Should not render spinner and error on start', async () => {
-        const { getByTestId } = render(<Login/>)
+        const { getByTestId } = render(<Login/>)        
         const errorWrap = getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
     })
@@ -12,5 +13,14 @@ describe('Login Component', () => {
         const { getByTestId } = render(<Login/>)
         const submitButton = getByTestId('submit') as HTMLButtonElement
         expect(submitButton.disabled).toBe(true)
+    })
+    test('Should start inputs with initial states', async () => {
+        const { getByTestId } = render(<Login/>)
+        const emailStatus = getByTestId('email-status')
+        expect(emailStatus.title).toBe('Campo obrigatório')
+        expect(emailStatus.textContent).toBe('🔴')
+        const passwordStatus = getByTestId('password-status')
+        expect(passwordStatus.textContent).toBe('🔴')
+        expect(passwordStatus.title).toBe('Campo obrigatório')
     })
 })
